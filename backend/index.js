f5b2dada191abf61a320ae8ce4fb4fc7363cec7f
STIWARTs <stiwartsaxena477@gmail.com>
2025-06-05 23:30:08 +0530

    Setup Backend Folder Structure

--- backend/index.js
+++ backend/index.js
@@ -61,55 +61,9 @@ yargs(hideBin(process.argv)) // Initialize yargs, argv taken form command, hideB
 
   //// Function to start the server
 
-// function startServer() { // node index.js start
-//   console.log("Server logic called!");
-// }
-
-function startServer() {
-  const app = express();
-  const port = process.env.PORT || 3000;
-
-  app.use(bodyParser.json());
-  app.use(express.json());
-
-  const mongoURI = process.env.MONGODB_URI;
-
-  mongoose
-    .connect(mongoURI)
-    .then(() => console.log("MongoDB connected!"))
-    .catch((err) => console.error("Unable to connect : ", err));
-
-  app.use(cors({ origin: "*" }));
-
-  app.use("/", mainRouter);
-
-  let user = "test";
-  const httpServer = http.createServer(app);
-  const io = new Server(httpServer, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-    },
-  });
-
-  io.on("connection", (socket) => {
-    socket.on("joinRoom", (userID) => {
-      user = userID;
-      console.log("=====");
-      console.log(user);
-      console.log("=====");
-      socket.join(userID);
-    });
-  });
-
-  const db = mongoose.connection;
-
-  db.once("open", async () => {
-    console.log("CRUD operations called");
-    // CRUD operations
-  });
-
-  httpServer.listen(port, () => {
-    console.log(`Server is running on PORT ${port}`);
-  });
+function startServer() { // node index.js start
+  console.log("Server logic called!");
 }
+
+
+
