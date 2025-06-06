7c42a74b3a8920f2054778f85083d875a090c75f
STIWARTs <stiwartsaxena477@gmail.com>
2025-06-06 09:18:29 +0530

    Setting up Repo Functionality

---
+++ backend/controllers/repoController.js
@@ -0,0 +1,43 @@
+
+async function createRepository(req, res) {
+    res.send("Repository created successfully!");
+}
+
+async function getAllRepositories(req, res) {
+    res.send("All repositories fetched successfully!");
+}
+
+async function fetchRepositoryById(req, res) {
+    res.send("Repository fetched by ID successfully!");
+}
+
+async function fetchRepositoryByName(req, res) {
+    res.send("Repository fetched by name successfully!");
+}
+
+async function fetchRepositoriesForCurrentUser(req, res) {
+    res.send("Repositories for current user fetched successfully!");
+}
+
+async function updateRepositoryById(req, res) {
+    res.send("Repository updated by ID successfully!");
+}
+
+async function toggleVisibilityById(req, res) { //for making a repository public or private
+    res.send("Repository visibility toggled successfully!");
+}
+
+async function deleteRepositoryById(req, res) {
+    res.send("Repository deleted by ID successfully!");
+}
+
+module.exports = {
+  createRepository, //only authenticated user can access this route
+  getAllRepositories,
+  fetchRepositoryById, 
+  fetchRepositoryByName, 
+  fetchRepositoriesForCurrentUser, // 
+  updateRepositoryById, //
+  toggleVisibilityById,
+  deleteRepositoryById, //
+};
---
+++ backend/routes/repo.router.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const repoController = require("../controllers/repoController");
+
+const repoRouter = express.Router();
+
+repoRouter.post("/repo/create", repoController.createRepository);
+repoRouter.get("/repo/all", repoController.getAllRepositories);
+repoRouter.get("/repo/:id", repoController.fetchRepositoryById);
+repoRouter.get("/repo/name/:name", repoController.fetchRepositoryByName);
+repoRouter.get("/repo/user/:userID", repoController.fetchRepositoriesForCurrentUser);
+repoRouter.put("/repo/update/:id", repoController.updateRepositoryById); //put for multiple fields update
+repoRouter.delete("/repo/delete/:id", repoController.deleteRepositoryById);
+repoRouter.patch("/repo/toggle/:id", repoController.toggleVisibilityById); //patch for individual field update
+
+module.exports = repoRouter;
