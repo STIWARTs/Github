29fea64f38d5bc5b5aafe6f17db23551fc52fcbf
STIWARTs <stiwartsaxena477@gmail.com>
2025-06-07 23:01:26 +0530

    Building the Signup & Login Functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,10 +7,12 @@ import ProjectRoutes from './Routes.jsx';
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <AuthProvider> // Provide the AuthContext to the entire app
+  // Provide the AuthContext to the entire app
+  <AuthProvider> 
     <Router> 
       {/* <App /> */}
-      <ProjectRoutes />//This compo decides/controls which route user to go,,//To check if the user is authenticated and to handle routing//1st loading component that will be rendered when the app loads--controlles rest o the routing of the app based on the user's authentication status
+      {/* //This compo decides/controls which route user to go,,//To check if the user is authenticated and to handle routing//1st loading component that will be rendered when the app loads--controlles rest o the routing of the app based on the user's authentication status */}
+      <ProjectRoutes />
     </Router>
   </AuthProvider>
 );
